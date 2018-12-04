20c5341fd8e2ce3326ded236137c0e7650949652
김 남규 <skarb629@gmail.com>
2018-12-04 15:34:52 +0900

    수업내용 3시

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using dotnetwebapi.Modules;
+using MySql.Data.MySqlClient;
+using System.Collections;
+
 
 namespace dotnetwebapi.Controllers
 {
@@ -12,33 +16,30 @@ namespace dotnetwebapi.Controllers
     {
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<ArrayList> Get()
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
+            MYsql my = new MYsql();
+            string sql = "select * from test;";
+            ArrayList list = new ArrayList();
+            MySqlDataReader sdr = my.Reader(sql);
+            while(sdr.Read()) //행 반복
+            {
+                Hashtable ht = new Hashtable(); //열 담기
+                for( int i = 0; i<sdr.FieldCount; i++) 
+                {
+                    ht.Add(sdr.GetName(i),sdr.GetValue(i));
+                }
+                //어딘가에 담아야된다.
+                list.Add(ht);
+            }
+             my.ReaderClose(sdr);
+            my.ConnectionClose();
+            return list;
         }
 
         // POST api/values
         [HttpPost]
         public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
         {
         }
     }
